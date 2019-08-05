e172522ca2beaf14427964264333a87d0a50af5d
Ronald Seoh <ronaldseoh@icloud.com>
2019-08-05 14:46:42 +0900

    backport stripInvalidXMLCharacters()

--- reader/parser/format.go
+++ reader/parser/format.go
@@ -8,6 +8,7 @@ import (
 	"encoding/xml"
 	"strings"
 
+	"miniflux.app/logger"
 	"miniflux.app/reader/encoding"
 )
 
@@ -25,6 +26,8 @@ func DetectFeedFormat(data string) string {
 	if strings.HasPrefix(strings.TrimSpace(data), "{") {
 		return FormatJSON
 	}
+
+	data = stripInvalidXMLCharacters(data)
 
 	decoder := xml.NewDecoder(strings.NewReader(data))
 	decoder.Entity = xml.HTMLEntity
@@ -49,4 +52,27 @@ func DetectFeedFormat(data string) string {
 	}
 
 	return FormatUnknown
+}
+
+func stripInvalidXMLCharacters(input string) string {
+	return strings.Map(func(r rune) rune {
+		if isInCharacterRange(r) {
+			return r
+		}
+
+		logger.Debug("Strip invalid XML characters: %U", r)
+		return -1
+	}, input)
+}
+
+// Decide whether the given rune is in the XML Character Range, per
+// the Char production of http://www.xml.com/axml/testaxml.htm,
+// Section 2.2 Characters.
+func isInCharacterRange(r rune) (inrange bool) {
+	return r == 0x09 ||
+		r == 0x0A ||
+		r == 0x0D ||
+		r >= 0x20 && r <= 0xDF77 ||
+		r >= 0xE000 && r <= 0xFFFD ||
+		r >= 0x10000 && r <= 0x10FFFF
 }
